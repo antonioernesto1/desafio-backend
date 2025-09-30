800adc56fcfdf7f6e8dc70d97b3df42ed41c5507
Antonio Santos <antonioernestodossantos@gmail.com>
2025-09-30 20:02:17 -0300

    feat: Creating interfaces for repositories

---
+++ MotorcycleRental/src/Core/MotorcycleRental.Domain/Interfaces/Repositories/IDeliveryDriverRepository.cs
@@ -0,0 +1,13 @@
+﻿using MotorcycleRental.Domain.Aggregates.DeliveryDriver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MotorcycleRental.Domain.Interfaces.Repositories
+{
+    public interface IDeliveryDriverRepository : IRepository<DeliveryDriver>
+    {
+    }
+}
---
+++ MotorcycleRental/src/Core/MotorcycleRental.Domain/Interfaces/Repositories/IRentalRepository.cs
@@ -0,0 +1,13 @@
+﻿using MotorcycleRental.Domain.Aggregates.Rental;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MotorcycleRental.Domain.Interfaces.Repositories
+{
+    public interface IRentalRepository : IRepository<Rental>
+    {
+    }
+}
