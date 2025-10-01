2f0a35667a9666e00af47207487c74d2a68ea7de
Antonio Santos <antonioernestodossantos@gmail.com>
2025-09-30 22:01:16 -0300

    feat: Creating database context for Postgres connection

---
+++ MotorcycleRental/src/Infrastructure/MotorcycleRental.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MotorcycleRental.Infrastructure.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MotorcycleRental.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("Default");
+            services.AddDbContext<AppDbContext>(x 
+                => x.UseNpgsql(connectionString)
+                    .UseSnakeCaseNamingConvention());
+        }
+    }
+}
---
+++ MotorcycleRental/src/Infrastructure/MotorcycleRental.Infrastructure/Persistence/AppDbContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using MotorcycleRental.Domain.Aggregates.DeliveryDriver;
+using MotorcycleRental.Domain.Aggregates.Motorcycle;
+using MotorcycleRental.Domain.Aggregates.Rental;
+
+namespace MotorcycleRental.Infrastructure.Persistence
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Rental> Rentals { get; set; }
+        public DbSet<Motorcycle> Motorcycles { get; set; }
+        public DbSet<DeliveryDriver> DeliveryDrivers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<DeliveryDriver>(dd =>
+            {
+                dd.OwnsOne(d => d.Cnh);
+
+                dd.HasIndex(d => d.Cnpj).IsUnique();
+                dd.HasIndex(d => d.Cnh.CnhNumber).IsUnique();
+            });
+
+            modelBuilder.Entity<Motorcycle>()
+                .HasIndex(m => m.LicensePlate)
+                .IsUnique();
+        }
+    }
+}
